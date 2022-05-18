e4f79c6d17739472f52e146416d2e64db5fb5bb2
Lahoucine Hamsek <49314721+RIIDLEY@users.noreply.github.com>
2022-05-18 14:58:40 +0200

    Changer place d'une image

--- Views/view_end.php
+++ Views/view_end.php
@@ -1,8 +1,9 @@
 </main>
 </body>
 <div class="signature">
-    <img src="Assets/img/logo_paragraphe.png" width="10%">
     <img src="Assets/img/paris8.jpg" width="10%">
+    <img src="Assets/img/logo_paragraphe.png" width="10%">
+
 </div>
 
 </html>
